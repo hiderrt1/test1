ea42a9c825aff71df59628d85f9beaaa42ee21c0
hiderrt1 <sandy.hider@jhuapl.edu>
2014-05-28 17:46:43 -0400

    first commit.

---
+++ src/main/java/test1/HelloTest2.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2012 The Johns Hopkins University/Applied Physics Laboratory.
+ * All rights reserved.  This material may be used, modified, or reproduced by or 
+ * for the U.S. Government pursuant to the license rights granted under the clauses
+ * at DFARS 252.227-7013/7014.  For any other permissions, please contact the Science
+ * and Technology Business Area Program Office at JHU/APL.
+ */
+package test1;
+
+public class HelloTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub World 2.");
+    }
+
+}
